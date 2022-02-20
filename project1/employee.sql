f8dca6c2244cc50958571d1192f890d83454769c
swathipoojary99 <98868484+swathipoojary99@users.noreply.github.com>
2022-02-20 11:41:16 +0530

    Delete employee.sql

--- project1/employee.sql
+++
@@ -1,9 +0,0 @@
-SELECT * FROM employee.emp;
-SELECT emp_no, name, designation from employee.emp;
-select * from employee.emp where gender='f';
-select distinct department from employee.emp;
-select emp_no,name,Designation from employee.emp where salary>15000;
-select name from employee.emp where salary in(select max(salary)from employee.emp);
-select Department,count(*)from employee.emp where department in('Marketing','Sales') group by department;
-select department,avg(salary),max(salary),min(salary) from employee.emp group by department;
-select department from employee.emp group by department having count(*)>2;
